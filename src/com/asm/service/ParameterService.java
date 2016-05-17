b0742037cf784c2653499de3a9fbe698b396b802
JokeAliens <zyj83737654@hotmail.com>
2016-05-17 22:05:06 +0800

    郑用杰-201605172205

---
+++ src/com/asm/service/ParameterService.java
@@ -0,0 +1,11 @@
+package com.asm.service;
+
+import java.util.List;
+
+import com.asm.domain.Parameter;
+
+public interface ParameterService {
+	public abstract Parameter checkparameterExist(String parameterName);
+	public abstract void updateParamter(Parameter parameter);
+	public abstract List<Parameter> scanTable();
+}
